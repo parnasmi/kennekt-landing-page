b38d71bd8871ac2ad39a98cc1259b51db3cd7721
Ilhom Maksadkulov <parnasmi@gmail.com>
2021-12-15 17:40:26 +0500

    feat: click everywhere to close menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
 (function () {
 	const hamburgerBtn = document.querySelector(".hamburger-btn");
 	const navMenu = document.querySelector(".page-header__nav");
-
 	hamburgerBtn.addEventListener("click", function (e) {
 		hamburgerBtn.classList.toggle("opened");
 		navMenu.classList.toggle("opened");
+	});
+	// Click everywhere to close navigation
+	document.addEventListener("click", function (event) {
+		if (navMenu.contains(event.target) || hamburgerBtn.contains(event.target)) {
+			return;
+		}
+		if (!navMenu.contains(event.target) || !hamburgerBtn.contains(event.target)) {
+			navMenu.classList.remove("opened");
+			hamburgerBtn.classList.remove("opened");
+		}
 	});
 })();
